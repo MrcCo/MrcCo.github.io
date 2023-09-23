fb4573e21ece1188e2df462ef41e3238a10393ec
Mihajlo Kostic <mihajlo.kostic25200@gmail.com>
2023-09-23 18:51:18 +0200

    Animate hamburger menu icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import NavStyles from "./NavStyles.module.css";
-import { LuMenu } from "react-icons/lu";
+import { BurgerClose } 
+  from "react-burger-icons";
 
 export const Navbar = () => {
 	const [toggleMenu, setToggleMenu] = useState(false);
@@ -15,40 +16,48 @@ export const Navbar = () => {
 		<div
 			className={`bg-base-100 w-full max-h-max items-center grid grid-cols-[1fr_1fr] px-32 pt-14 overflow-x-hidden max-[1500px]:px-16 max-[800px]:px-6 ${NavStyles.nav_container}`}
 		>
-			<h1 className={`normal-case text-4xl w-max font-bold text-[#eaeaea] max-[600px]:text-3xl max-[400px]:text-2xl`}>
+			<h1
+				className={`normal-case text-4xl w-max font-bold text-[#eaeaea] max-[600px]:text-3xl max-[400px]:text-2xl`}
+			>
 				Marko Stefanović
 			</h1>
 			<div className="place-self-end">
-			<button className="btn btn-ghost place-self-end p-0 z-10" type="button" onClick={handleToggleMenu}>
-				<LuMenu size={36} />
-			</button>
-			<div
-				className={`flex flex-col absolute top-28 translate-x-[-20px] ${NavStyles.menu}`}
-				data-toggle={toggleMenu ? "true" : "false"}
-			>
-				<NavLink
-					to="/"
-					className={({ isActive, isPending }) =>
-						`btn hover:bg-transparent hover:text-[#eaeaea] p-0 bg-transparent border-none normal-case text-xl max-[600px]:text-lg ${
-							NavStyles.navlink
-						} ${isActive ? "text-[#eaeaea]" : " text-slate-400"}`
-					}
-					disabled={toggleMenu ? false : true}
+				
+					<button
+						className="btn btn-ghost place-self-end p-0 z-10"
+						type="button"
+						onClick={handleToggleMenu}
+					>
+						<BurgerClose isClosed={toggleMenu} />
+					</button>
+				
+				<div
+					className={`flex flex-col absolute top-28 translate-x-[-20px] ${NavStyles.menu}`}
+					data-toggle={toggleMenu ? "true" : "false"}
 				>
-					About
-				</NavLink>
-				<NavLink
-					to="/blog"
-					className={({ isActive, isPending }) =>
-						`btn hover:bg-transparent hover:text-[#eaeaea] p-0 bg-transparent border-none normal-case text-xl max-[600px]:text-lg ${
-							NavStyles.navlink
-						} ${isActive ? "text-[#eaeaea]" : "text-slate-400"}`
-					}
-					disabled={toggleMenu ? false : true}
-				>
-					Blog
-				</NavLink>
-			</div>
+					<NavLink
+						to="/"
+						className={({ isActive, isPending }) =>
+							`btn hover:bg-transparent hover:text-[#eaeaea] p-0 bg-transparent border-none normal-case text-xl max-[600px]:text-lg ${
+								NavStyles.navlink
+							} ${isActive ? "text-[#eaeaea]" : " text-slate-400"}`
+						}
+						disabled={toggleMenu ? false : true}
+					>
+						About
+					</NavLink>
+					<NavLink
+						to="/blog"
+						className={({ isActive, isPending }) =>
+							`btn hover:bg-transparent hover:text-[#eaeaea] p-0 bg-transparent border-none normal-case text-xl max-[600px]:text-lg ${
+								NavStyles.navlink
+							} ${isActive ? "text-[#eaeaea]" : "text-slate-400"}`
+						}
+						disabled={toggleMenu ? false : true}
+					>
+						Blog
+					</NavLink>
+				</div>
 			</div>
 		</div>
 	);
